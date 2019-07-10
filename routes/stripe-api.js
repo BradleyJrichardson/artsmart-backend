178ca2c2427515eddd33a22771290f9e396d5ff4
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-11 09:26:03 +1000

    refactoring stripe stuff

--- routes/stripe-api.js
+++ routes/stripe-api.js
@@ -1,51 +1,29 @@
 const express = require("express");
 const router = express.Router();
-// const User = require("../models/User");
 const stripe = require("stripe")("sk_test_PAGemW3HJHwcvA0PLP6eme6F00nK93R7T3");
 
-/// https://stripe.com/docs/recipes/elements-react
-
-/// create a customer
-//stripe.com/docs/api/customers
-
-/// create an order
 router.post("/order", async (req, res) => {
   const order = req.body.order;
   const source = req.body.source;
   const { email } = req.body.order;
   const { name } = req.body.order.shipping;
+  console.log(name);
 
   try {
-    // let stripeCustomer = null;
-    // const stripeOrder = await stripe.orders.create(order);
-    // stripeCustomer = await stripe.customers.create({
-    //   email: email,
-    //   description: name,
-    //   source: source
-    // });
-
+    let stripeCustomer;
+    const stripeOrder = await stripe.orders.create(order);
     let existingCustomers = await stripe.customers.list({ email: email });
     if (existingCustomers.data.length) {
       console.log("customer exists!");
     } else {
-      let customer = await stripe.customers.create({
+      let stripeCustomer = await stripe.customers.create({
         email: email,
         description: name,
         source: source
       });
+      console.log(`Customer created: ${stripeCustomer.description}`);
     }
-    // stripe.customers.retrieve("cus_FPNB4FFp7v481M", async (err, customer) => {
-    //   stripeCustomer = customer;
-    //   if (stripeCustomer === null) {
-    //     stripeCustomer = await stripe.customers.create({
-    //       email: email,
-    //       description: name,
-    //       source: source
-    //     });
-    //     console.log(`Customer created: ${stripeCustomer.description}`);
-    //   }
-    // });
-    console.log(`Customer created: ${stripeCustomer.description}`);
+
     console.log(`Order created: ${stripeOrder.id}`);
   } catch (err) {
     console.log(`Order error: ${err}`);
